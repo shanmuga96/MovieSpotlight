f60ca08c230046987f535c830834571d84e23fdc
shanmuga96 <shanmugaperumal96@gmail.com>
2025-09-13 01:35:31 +0530

    Initial Commit for running the spring boot application

---
+++ src/test/java/com/shanmuga/moviespotlight/MoviespotlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanmuga.moviespotlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviespotlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
